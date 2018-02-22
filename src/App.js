c3aa01afd205d075340fab8c1ce63afdcd2944d6
github username <github email address>
2018-02-21 19:08:39 -0500

    iphone message

--- src/App.js
+++ src/App.js
@@ -35,13 +35,15 @@ class App extends Component {
   )
 ) {
   return (
-    <div className="flex items-center justify-between text-center bg-teal p-2" >
+    <div className="flex items-center justify-between text-center text-lg bg-teal p-2 h-full" >
+    <div>
     <div className="flex px-4 py-2 items-center flex-no-shrink text-white">
       <svg className="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
       <span className="font-semibold text-2xl tracking-tight">Vapor paint</span>
     </div>
+    </div>
     <div>
-      <h1 className="bg-teal-lightest border-t-4 border-teal rounded-b text-teal-darkest px-4 py-3 shadow-md font-bold" >
+      <h1 className="rounded-b text-white px-4 py-3 shadow-md font-bold" >
         Because of the graphic heavy nature of vapor paint, it cannot
         be viewed on mobile devices. Please come back on your laptop or
         desktop computer!
