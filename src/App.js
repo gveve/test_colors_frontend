247c746b32ebdbf3e9a04f468a927e80cef1ce48
github username <github email address>
2018-02-21 19:01:08 -0500

    teal

--- src/App.js
+++ src/App.js
@@ -35,12 +35,18 @@ class App extends Component {
   )
 ) {
   return (
-    <div style={{ textAlign: "center", padding: "3%" }}>
+    <div className="flex items-center justify-between text-center bg-teal p-2" >
+    <div className="flex px-4 py-2 items-center flex-no-shrink text-white">
+      <svg className="fill-current h-8 w-8 mr-2" width="54" height="54" viewBox="0 0 54 54" xmlns="http://www.w3.org/2000/svg"><path d="M13.5 22.1c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05zM0 38.3c1.8-7.2 6.3-10.8 13.5-10.8 10.8 0 12.15 8.1 17.55 9.45 3.6.9 6.75-.45 9.45-4.05-1.8 7.2-6.3 10.8-13.5 10.8-10.8 0-12.15-8.1-17.55-9.45-3.6-.9-6.75.45-9.45 4.05z"/></svg>
+      <span className="font-semibold text-2xl tracking-tight">Vapor paint</span>
+    </div>
+    <div>
       <h1 className="bg-teal-lightest border-t-4 border-teal rounded-b text-teal-darkest px-4 py-3 shadow-md font-bold" >
         Because of the graphic heavy nature of vapor paint, it cannot
         be viewed on mobile devices. Please come back on your laptop or
         desktop computer!
       </h1>
+    </div>
     </div>
   )
   } else {
